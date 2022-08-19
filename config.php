c01e72928a5cb172112bd4d429a44b994281697a
VigularIgnat <88516059+VigularIgnat@users.noreply.github.com>
2022-08-20 00:31:07 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php 
 
-    $host="localhost";
-    $user="root";
-    $password="";
-    $db="bankomat";
+    $user="atmihnat_user";
+    $password="0000user";
+    $db="atmihnat_bankomat2";
     $options_admin=[
         0=> "not active",
         1=> "admin",
